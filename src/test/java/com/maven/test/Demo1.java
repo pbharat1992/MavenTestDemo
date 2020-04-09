55d95cf7d7f1a2b0fe00107be18527977ac94d9f
bharat <you@example.com>
2020-04-09 11:55:52 +0530

    maven practice program

---
+++ src/test/java/com/maven/test/Demo1.java
@@ -0,0 +1,42 @@
+package com.maven.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+
+	@Test
+	public void sum() {
+		System.out.println("SUM");
+        int a = 10;
+		int b = 20;
+		Assert.assertEquals(30, a + b);	}
+
+	@Test
+	public void sub() {
+		System.out.println("SUBTRACTION");
+        int a = 10;
+		int b = 20;
+		Assert.assertEquals(10, b-a);
+	}
+
+	@Test
+	public void div() {
+		System.out.println("DIVISION");
+        int a = 10;
+		int b = 20;
+		Assert.assertEquals(2,b/a);
+	}
+
+	@Test
+	public void mul() {
+		System.out.println("MULTIPLICATION");
+        int a = 10;
+		int b = 20;
+		Assert.assertEquals(200, a * b);
+	}
+
+
+
+}
